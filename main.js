b8af658d4ccd62e2e8cb5fd9ee81b553da9d2d08
Kevin Perez <kevinperez@knights.ucf.edu>
2021-04-01 16:59:27 -0400

    Setup main

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('KaeBot is online');
+});
+
+
+
+
+client.login('ODI3MjgzODgxOTg3NjcwMDQ2.YGYyJw.vmmXHtiAQx94nzXblx_nYaebuP0');
